6449843fc19bae08f170982adab81eb4e6a02d0d
Aritra <aritra.nag@gmail.com>
2024-09-17 01:03:21 +0530

    HTTP/1.1 support added, Exercise 1, chapter 1

--- index.py
+++ index.py
@@ -4,15 +4,11 @@ import url.myUrl as myUrl
 
 urlString = "http://example.org/index.html"
 
-def load(URL):
-    '''
-    Requests the URL and then calls show to print the text returned
-    '''
-    response = URL.request()
-    URL.show(response["content"])
+
 
 if __name__ == "__main__":
-    load(myUrl.URL(sys.argv[1]))
+    myUrl.load(sys.argv[1])
+    #help(myUrl.URL.request)
 
 
     
--- url/myUrl.py
+++ url/myUrl.py
@@ -4,15 +4,20 @@ import socket
 import ssl
 
 class URL:
-    '''
+    """
     URL class to work with a URL
+
+    Method List:
     constructor - parses the URL string into scheme, host and path
-    '''
+    request - Creates a socket object to connect to the server and make a request call based on scheme, port and path. Returns the response as an object
+    _createRequest - Utility function to create the necessary request
+    _parseResponse - Utility function to parse the response into an object format for later use
+    """
     def __init__(self,url):
-        '''
+        """
         @param: url - URL of the website we are trying to go to
         parses the URL into scheme/protocol,port, host and path. stores them as object properties
-        '''
+        """
         self.scheme,url = url.split("://",1)
         
         #checking the fact that our browser only supports http / https
@@ -41,9 +46,9 @@ class URL:
 
 
     def request(self):
-        '''
+        """
         Download the web page defined by the host and path values in the class object
-        '''
+        """
         # create a socket to connect to the host 
         # socket connection details = family=address family, defines location(where), type=stream/datagran(what), proto=(how)
         s = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM,proto=socket.IPPROTO_TCP)
@@ -58,13 +63,7 @@ class URL:
             _ctx = ssl.create_default_context()
             s = _ctx.wrap_socket(s,server_hostname=self.host)
 
-        # REQUEST being created for sending
-        #GET path HTTP/1.0
-        #Host: hostname
-        #empty line to indicate end of the input
-        request = "GET {} HTTP/1.0\r\n".format(self.path)
-        request += "Host: {}\r\n".format(self.host)
-        request += "\r\n"
+        request = self._createRequest()
         s.send(request.encode('utf8')) #encode the request in utf8 format to create a byte stream to send
         #TODO : understand the nuances of the encoder to use for real world use case
 
@@ -72,35 +71,38 @@ class URL:
         # originally we have to use a loop to read all the bytes coming to the socket from the server
         # makefile utility in python is a shortcut to read all the bytes, decode it in the proper format and even accommodate HTTP line endings
         response = s.makefile('r',encoding="utf8",newline="\r\n")
-        responseObj = self.parseResponse(response)
+        responseObj = self._parseResponse(response)
         s.close()
 
         return responseObj
     
 
-    def show(self,body):
-        '''
-        A simple HTML parser, shows all the text in the HTML, but none of the tags
-        '''
-        _in_tag = False
-        # goes through each character in the body
-        # determines whether the character is part of a tag
-        # if not, the character is printed
-        for c in body:
-            if c == '<':
-                _in_tag = True
-            elif c == '>':
-                _in_tag = False
-            elif not _in_tag:
-                print(c,end="")
-                     
+    def _createRequest(self):
+        """
+        Create request string to send to the server
 
+        Response:
+        request - Complete Request string with all the headers added
+        """
+        # REQUEST being created for sending
+        #GET path HTTP/1.0
+        #Host: hostname
+        #User-Agent : python/3
+        #Connection : Close
+        #empty line to indicate end of the input
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "User-Agent: {}\r\n".format("python/3")
+        request += "Connection: close\r\n"
+        request += "\r\n"
+        return request
+    
 
-    def parseResponse(self, response):
-        '''
+    def _parseResponse(self,response):
+        """
         Utility function for parsing the response (response must be a an object of makefile)
         Creates a dictionary with all the parts of the response and returns it
-        '''
+        """
         _r = {}
 
         #1st line of the response is the status line containing version,status,explanation
@@ -127,4 +129,28 @@ class URL:
         return _r
 
                                         
+def load(url):
+    """
+    Requests the URL and then calls show to print the text returned
+    """
+    newUrl = URL(url)
+    response = newUrl.request()
+    show(response["content"])
+
+def show(body):
+    """
+    A simple HTML parser, shows all the text in the HTML, but none of the tags
+    """
+    _in_tag = False
+    # goes through each character in the body
+    # determines whether the character is part of a tag
+    # if not, the character is printed
+    for c in body:
+        if c == '<':
+            _in_tag = True
+        elif c == '>':
+            _in_tag = False
+        elif not _in_tag:
+            print(c,end="")
+
 
